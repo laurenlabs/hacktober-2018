3a0117759ee7bd597ae33201317a35ab328e2308
lmariel <laurenmariel90@gmail.com>
2018-10-07 18:01:11 -0400

    Fixed spelling

--- pages/Hacktoberfest.jsx
+++ pages/Hacktoberfest.jsx
@@ -3,8 +3,8 @@ import Page from '../components/Page';
 
 const Index = () => (
   <Page>
-    <p>HAPPY HACKING HACKTOBERFES 2K18</p>
-    <img src='https://user-images.githubusercontent.com/121322/45653341-78d1c000-ba8c-11e8-9497-2855130c0634.png' />
+    <p>HAPPY HACKING HACKTOBERFEST 2K18</p>
+    <img src="https://user-images.githubusercontent.com/121322/45653341-78d1c000-ba8c-11e8-9497-2855130c0634.png" />
   </Page>
 );
 
