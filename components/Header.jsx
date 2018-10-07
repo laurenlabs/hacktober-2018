0e36650c9caf3ca50059cc697c1b088a25c23a6a
Talita Oliveira <litaa.oliveira@gmail.com>
2018-10-07 16:52:11 -0300

    Style header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import Link from 'next/link';
 
+const style = {
+  backgroundColor: '#2c80b9',
+  height: '68px',
+  display: 'flex',
+  alignItems: 'center',
+  paddingLeft: '20px',
+  fontFamily: 'Arial'
+}
+
+const styleLink = {
+  marginRight: '20px',
+  color: '#FFF',
+  textDecoration: 'none',
+  padding: '10px',
+  borderRight: '1px solid #FFF'
+}
+
 const Header = () => (
-  <div>
+  <div style={style}>
     <Link href="/">
-      <a style={{ marginRight: '20px' }}>Home</a>
+      <a style={styleLink}>Home</a>
     </Link>
     <Link href="/random-users">
-      <a style={{ marginRight: '20px' }}>API - Random Users</a>
+      <a style={styleLink}>API - Random Users</a>
     </Link>
   </div>
 );
