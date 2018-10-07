d1290352f968e58d5e588279dd9e75eac5f2696f
gh05d <pc@vipfy.com>
2018-10-07 13:21:50 +0200

    added footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 const Footer = () => (
-  <div>
+  <section id="footer">
     <p>This is the footer.</p>
-  </div>
+    <p>
+      <a
+        href="https://hacktoberfest.digitalocean.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Hacktoberfest 2018
+      </a>
+    </p>
+    <p>
+      <span>&#169;</span>
+      <a
+        href="https://github.com/lmariel"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        lmariel
+      </a>
+    </p>
+  </section>
 );
 
 export default Footer;
